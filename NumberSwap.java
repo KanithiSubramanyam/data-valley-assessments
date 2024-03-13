d6dcec8a8be187209958c996e5fcdaacbbc69a2c
KantihiSubramanyam <subramanyamkanithi@gmail.com>
2024-03-13 15:57:27 +0530

    conflict resolved

--- NumberSwap.java
+++ NumberSwap.java
@@ -5,7 +5,6 @@ public class NumberSwap {
 
         System.out.println("Before swapping: a = " + a + ", b = " + b);
 
-        // Swap a and b without using a third variable and without using + or - operators
         a = a ^ b;
         b = a ^ b;
         a = a ^ b;
